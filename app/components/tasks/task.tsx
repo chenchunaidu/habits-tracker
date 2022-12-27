34685896b9aeb79329c5a68c7bc59860eb112e8f
Chenchunaidu <maddinenichenchunaidu@gmail.com>
2022-12-27 16:16:34 +0530

    fix: fixed ui

--- app/components/tasks/task.tsx
+++ app/components/tasks/task.tsx
@@ -18,9 +18,12 @@ const Task: React.FC<TaskProps> = (task) => {
       className={`rounder-md space-y-4 rounded-md bg-lime-300 py-8 px-4 text-stone-600 shadow-md`}
     >
       <div className="space-y-1">
-        <div className="flex justify-between">
-          <div className="text-lg font-semibold text-stone-700">
-            {task.title}
+        <div className="flex items-start justify-between">
+          <div className="flex space-x-2">
+            <input type="checkbox" className="mt-2 h-4 w-4" />
+            <div className="text-lg font-semibold text-stone-700">
+              {task.title}
+            </div>
           </div>
           <Link to={`/home/tasks/${task.id}/subtask/new`}>
             <Button variant="ghost" size="sm">
@@ -35,9 +38,6 @@ const Task: React.FC<TaskProps> = (task) => {
           <Subtask key={id} title={title} description={description} id={id} />
         );
       })}
-      <Button size="md" variant="ghost" className="w-full">
-        Mark as done
-      </Button>
     </div>
   );
 };
