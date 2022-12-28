f0199d267752ae1852d1df2a9bd9c2e1e4d74310
Chenchunaidu <maddinenichenchunaidu@gmail.com>
2022-12-28 10:23:12 +0530

    fix: added empty to tasks

--- app/routes/home/tasks/index.tsx
+++ app/routes/home/tasks/index.tsx
@@ -1,9 +1,10 @@
-import { Link, useLoaderData } from "@remix-run/react";
+import { useLoaderData } from "@remix-run/react";
 import type { LoaderFunction } from "@remix-run/server-runtime";
-import Button from "~/components/common/button";
+import Empty from "~/components/common/empty";
 import NavFloater from "~/components/habits/nav-floater";
 import HomeContainer from "~/components/home/home-container";
-import Task, { TaskProps } from "~/components/tasks/task";
+import type { TaskProps } from "~/components/tasks/task";
+import Task from "~/components/tasks/task";
 import { requiredUser } from "~/lib/auth/auth";
 import { getTasksByUserId } from "~/models/task.server";
 
@@ -21,15 +22,24 @@ export default function Homepage() {
   const { tasks } = useLoaderData<loaderResponse>();
   return (
     <HomeContainer header="Tasks" addLink="/home/tasks/new">
-      {tasks.map(({ subtasks, title, description, id }) => (
-        <Task
-          title={title}
-          description={description}
-          id={id}
-          subtasks={subtasks}
-          key={id}
+      {tasks.length ? (
+        tasks.map(({ subtasks, title, description, id, completed }) => (
+          <Task
+            title={title}
+            description={description}
+            id={id}
+            subtasks={subtasks}
+            key={id}
+            completed
+          />
+        ))
+      ) : (
+        <Empty
+          buttonLabel="Add new task"
+          buttonLink="/home/tasks/new"
+          label="You have no tasks"
         />
-      ))}
+      )}
       <NavFloater />
     </HomeContainer>
   );
